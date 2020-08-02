b34ff75eab74ba9abcda60ecf83b1a352d3ea597
Nikita Glazkov <zitrnik@gmail.com>
2020-08-02 21:13:35 +0300

    DEV: add i18n template with example

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <template>
   <div id="app">
-    <h1>Начало Фишки</h1>
+    <div class="locales">
+      <a @click="setLocale('en')"><flag iso="us"></flag></a>
+      <a @click="setLocale('ru')"><flag iso="ru"></flag></a>
+    </div>
+    <h1>{{ $t("welcomeMsg") }}</h1>
     <router-view></router-view>
   </div>
 </template>
 
+<script>
+export default {
+  methods: {
+    setLocale(locale) {
+      this.$i18n.locale = locale;
+    },
+  },
+};
+</script>
+
 <style lang="scss">
 h1 {
   color: purple;
+}
+h1,
+p {
+  font-family: sans-serif;
+}
+.locales {
+  display: flex;
+  a {
+    margin-right: 1rem;
+    display: block;
+    cursor: pointer;
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
 }
 </style>
