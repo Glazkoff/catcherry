71157f9dd4d3d7876f84bcec39a837cc1539a98b
Olga Krasnova <krasolga2018@gmail.com>
2020-10-13 12:40:52 +0300

    DEV: create a component OnePostList and start work on it

---
+++ src/components/OnePostList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "OnePostList",
+//   data() {
+//     return {};
+//   },
+  methods: {},
+};
+</script>
+
+<style></style>
