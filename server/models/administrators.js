0000a91305511538ed2d5b226821870f2ac952ab
Olga Krasnova <krasolga2018@gmail.com>
2020-10-16 20:07:04 +0300

    FIX: fix models

--- server/models/administrators.js
+++ server/models/administrators.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
   Administrators.init(
     {
       userId: DataTypes.INTEGER,
-      allowNull: false,
+      // allowNull: false,
     },
     {
       sequelize,
