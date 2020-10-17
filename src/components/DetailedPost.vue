c7b1a33cb932fdef09162fe52215d6084d310e5f
Olga Krasnova <krasolga2018@gmail.com>
2020-10-17 17:19:51 +0300

    DEV: output body post and comments (client)

--- src/components/DetailedPost.vue
+++ src/components/DetailedPost.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="container">
     <img src="../assets/placeholder.png" alt="placeholder" />
-    <h2>{{ this.headline }}</h2>
-    <p>{{ this.date }}</p>
-    <p>
-      {{ this.body }}
-    </p>
-    <button v-on:click="onLike()">Лайк</button>
-    <button v-on:click="onComment()">Коммент</button>
-    <button v-on:click="onRepost()">Репост</button>
+    <div class="postBody">
+      <h1>{{ this.post.headline }}</h1>
+      <p class="date">{{ this.post.date }}</p>
+      <p>
+        {{ this.post.body }}
+      </p>
+      <button v-on:click="onLike()">Лайк</button>
+      <button v-on:click="onComment()">Коммент</button>
+      <button v-on:click="onRepost()">Репост</button>
+      <div class="postComments">
+        <div v-for="comment in comments" :key="comment.id">
+          <p>{{ comment.author }}</p>
+          <p>{{ comment.body }}</p>
+        </div>
+        <form @submit.prevent="onAddComment">
+          <textarea
+            v-model="message"
+            rows="10"
+            placeholder="Добавить комментарий"
+          ></textarea>
+          <input type="submit" value="Добавить" />
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,11 +33,28 @@ export default {
   name: "DetailedPost",
   data() {
     return {
+      message: null,
       errors: [],
-      headline: "Новая срочная новость из отдела техподдержки",
-      date: "29.09.2020 17:00",
-      body:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae suscipit voluptate nemo. Perferendis, impedit! Quaerat nemo nulla soluta sunt accusamus in quae sed. Nam perferendis ratione totam autem minus quibusdam?",
+      post: {
+        headline: "Новая срочная новость из отдела техподдержки",
+        date: "29.09.2020 17:00",
+        body:
+          "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae suscipit voluptate nemo. Perferendis, impedit! Quaerat nemo nulla soluta sunt accusamus in quae sed. Nam perferendis ratione totam autem minus quibusdam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae suscipit voluptate nemo. Perferendis, impedit! Quaerat nemo nulla soluta sunt accusamus in quae sed. Nam perferendis ratione totam autem minus quibusdam?",
+      },
+      comments: [
+        {
+          id: 1,
+          author: "Олег Дубенский",
+          body:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae suscipit voluptate nemo. Perferendis, impedit! Quaerat nemo nulla soluta sunt accusamus in quae sed. Nam perferendis ratione totam autem minus quibusdam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae suscipit voluptate nemo. Perferendis, impedit! Quaerat nemo nulla soluta sunt accusamus in quae sed. Nam perferendis ratione totam autem minus quibusdam?",
+        },
+        {
+          id: 2,
+          author: "Олег Недубенский",
+          body:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae suscipit voluptate nemo. Perferendis, impedit! Quaerat nemo nulla soluta sunt accusamus in quae sed. Nam perferendis ratione totam autem minus quibusdam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae suscipit voluptate nemo. Perferendis, impedit! Quaerat nemo nulla soluta sunt accusamus in quae sed. Nam perferendis ratione totam autem minus quibusdam?",
+        },
+      ],
     };
   },
   methods: {
@@ -33,6 +66,9 @@ export default {
     },
     onRepost() {
       console.log("Нажата кнопка репоста");
+    },
+    onAddComment() {
+      console.log("Был оставлен комментарий: " + this.message);
     },
   },
 };
@@ -56,7 +92,32 @@ export default {
   display: block;
 }
 
+.postBody {
+  margin: 5%;
+}
+
+.postComments {
+  margin: 0 5% 0 5%;
+}
+
+.date {
+  text-align: right;
+}
+
+h1 {
+  text-align: center;
+}
+
+textarea {
+  width: 100%;
+}
+
 button {
-  margin: 0 1% 0 0;
+  margin: 0 1% 3% 0;
+}
+
+input[type="submit"] {
+  display: block;
+  margin: 0 0 0 auto;
 }
 </style>
