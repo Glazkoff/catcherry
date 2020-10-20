1e38ba0f82d274c0764b15afa09cbe603f178d8f
Nikita Glazkov <zitrnik@gmail.com>
2020-10-20 14:15:12 +0300

    FIX: cosmetic changes

--- src/views/AdminPanel.vue
+++ src/views/AdminPanel.vue
@@ -1,41 +1,39 @@
 <template>
-    <div class="adminPanel">
-        <div class="leftMenu">
-            <router-link class="a" to="/admin">Статистика системы</router-link>
-            <router-link class="a" to="/admin/users">Список пользователи</router-link>
-            <router-link to="/admin/organization">Список организаций</router-link>
-        </div>
-        <div class="main">
-            <router-view></router-view>
-        </div>
+  <div class="adminPanel">
+    <div class="leftMenu">
+      <router-link class="a" to="/admin">Статистика системы</router-link>
+      <router-link class="a" to="/admin/users">Список пользователи</router-link>
+      <router-link to="/admin/organization">Список организаций</router-link>
     </div>
+    <div class="main">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style lang="scss" scoped>
-    * {
-        box-sizing: border-box;
+* {
+  box-sizing: border-box;
+}
+.adminPanel {
+  display: flex;
+  width: 100%;
+  .leftMenu {
+    width: 20%;
+    padding: 1%;
+    border-right: 1px solid black;
+    .a {
+      display: block;
     }
-    .adminPanel {
-        display: flex;
-        width: 100%;
-        .leftMenu {
-            width: 20%;
-            padding: 1%;
-            border-right: 1px solid black;
-            .a {
-                display: block;
-            }
-        }
-        .main {
-            width: 80%;
-            padding: 1%;
-            overflow-x: hidden;
-        }
-    }
+  }
+  .main {
+    width: 80%;
+    padding: 1%;
+    overflow-x: hidden;
+  }
+}
 </style>
