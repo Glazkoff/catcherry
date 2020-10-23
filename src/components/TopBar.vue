0d250ac3cc79502db3fe780169871b0cc29fbee6
nvkolezneva <nvkolezneva@gmail.com>
2020-10-23 12:28:49 +0300

    DEV:work with vueclickaway

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -6,7 +6,7 @@
           <ul>
             <label for="gepleasurprised">
               <li class="icon right">
-                <a>☰</a>
+                <a v-on-clickaway="away">☰</a>
               </li>
             </label>
 
@@ -16,10 +16,22 @@
               style="display: none;"
             />
             <li class="nav-logo">CATCHERRY</li>
-            <li><router-link to="/user/156">{{ $t("accountMsg") }}</router-link></li>
-            <li><router-link to="/user/156/user_org">{{ $t("orgMsg") }}</router-link></li>
-            <li><router-link to="/user/156/list_req">{{ $t("applicationMsg") }}</router-link></li>
-            <li class="right"><button class="nav-exit">{{ $t("endMsg") }}</button></li>
+            <li>
+              <router-link to="/user/156">{{ $t("accountMsg") }}</router-link>
+            </li>
+            <li>
+              <router-link to="/user/156/user_org">{{
+                $t("orgMsg")
+              }}</router-link>
+            </li>
+            <li>
+              <router-link to="/user/156/list_req">{{
+                $t("applicationMsg")
+              }}</router-link>
+            </li>
+            <li class="right">
+              <button class="nav-exit">{{ $t("endMsg") }}</button>
+            </li>
             <li class="right ">
               <a class="nav-name"
                 >{{ $t("hellouserMsg") }},<br />
@@ -34,12 +46,19 @@
 </template>
 
 <script>
+import { directive as onClickaway } from "vue-clickaway";
 
 export default {
   name: "top-bar",
+  directives: {
+    onClickaway: onClickaway,
+  },
   methods: {
     setLocale(locale) {
       this.$i18n.locale = locale;
+    },
+    away: function() {
+      console.log("clicked away");
     },
   },
 };
