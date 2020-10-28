9840e69cc2831c910ff88fa39cd7f0566477909d
Olga Krasnova <krasolga2018@gmail.com>
2020-10-28 15:44:10 +0400

    dev: work on css in one post from list

--- src/components/OnePostList.vue
+++ src/components/OnePostList.vue
@@ -2,12 +2,15 @@
   <div class="container">
     <img src="../assets/placeholder.png" alt="placeholder" />
     <h2>{{ this.headline }}</h2>
-    <p>{{ this.date }}</p>
-    <p>
+    <p class="postDate">{{ this.date }}</p>
+    <p class="postBody">
       {{ this.body }}
     </p>
+    <span class="countLike">10</span>
     <button v-on:click="onLike()">Лайк</button>
+    <span class="countComment">999</span>
     <button v-on:click="onComment()">Коммент</button>
+    <span class="countRepost">9</span>
     <button v-on:click="onRepost()">Репост</button>
   </div>
 </template>
@@ -21,7 +24,7 @@ export default {
       headline: "Новая срочная новость из отдела техподдержки",
       date: "29.09.2020 17:00",
       body:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae suscipit voluptate nemo. Perferendis, impedit! Quaerat nemo nulla soluta sunt accusamus in quae sed. Nam perferendis ratione totam autem minus quibusdam?",
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae suscipit voluptate nemo. Perferendis, impedit! Quaerat nemo nulla soluta sunt accusamus in quae sed. Nam perferendis ratione totam autem minus quibusdam?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae suscipit voluptate nemo. Perferendis, impedit! Quaerat nemo nulla soluta sunt accusamus in quae sed. Nam perferendis ratione totam autem minus quibusdam?"
     };
   },
   methods: {
@@ -33,26 +36,45 @@ export default {
     },
     onRepost() {
       console.log("Нажата кнопка репоста");
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style scoped>
 .container {
-  border: 1px solid #b1b1b1;
+  /* border: 1px solid #b1b1b1; */
+  border-radius: 8px;
   margin: 5%;
   position: relative;
   box-sizing: border-box;
   overflow: hidden;
+  box-shadow: 0px 2px 8px rgba(40, 41, 61, 0.08),
+    0px 20px 32px rgba(96, 97, 112, 0.24);
 }
 
 .container img {
+  box-shadow: 0;
   margin-right: 5%;
   float: left;
   max-width: 30%;
   height: auto;
   display: block;
+}
+
+h2 {
+  color: purple;
+}
+
+p.postBody {
+  height: 5em;
+  overflow: hidden;
+  padding: 5px;
+  text-overflow: ellipsis;
+}
+
+span {
+  padding: 5px;
 }
 
 button {
