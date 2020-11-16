a194243961d8f4e8d3a38421a17dd212a64d3fe0
Fedor Gusev <gusevfedya@mail.ru>
2020-11-16 16:07:31 +0300

    Merge branch 'dev'

--- src/components/Manager/EditForm.vue
+++ src/components/Manager/EditForm.vue
@@ -1,18 +1,41 @@
 <template>
-<div>
-  <span>Последнее редактирование: {{ stampToDate(t.updatedAt) }} </span>
-  <form action="" @submit="$emit('update', name, description, parseInt(maxUsersLimit))">
-    <label for="name">Название</label>
-    <input type="text" name="name" class="form-control" placeholder="Название" v-model="name" />
-    <label for="description">Описание</label>
-    <textarea name="description" class="form-control" id="" cols="10" rows="5" placeholder="Описание" v-model="description"></textarea>
-    <label for="maxUsersLimit">Максимальное число пользователей</label>
-    <input type="number" name="maxUsersLimit" class="form-control" placeholder="Максимальное число участников" v-model="maxUsersLimit" />
-    <button type="submit" class="btn btn-primary">
-      Сохранить
-    </button>
-  </form>
-</div>
+  <div>
+    <span>Последнее редактирование: {{ $d(t.updatedAt, "long") }} </span>
+    <form
+      action=""
+      @submit="$emit('update', name, description, parseInt(maxUsersLimit))"
+    >
+      <label for="name">Название</label>
+      <input
+        type="text"
+        name="name"
+        class="form-control"
+        placeholder="Название"
+        v-model="name"
+      />
+      <label for="description">Описание</label>
+      <textarea
+        name="description"
+        class="form-control"
+        id=""
+        cols="10"
+        rows="5"
+        placeholder="Описание"
+        v-model="description"
+      ></textarea>
+      <label for="maxUsersLimit">Максимальное число пользователей</label>
+      <input
+        type="number"
+        name="maxUsersLimit"
+        class="form-control"
+        placeholder="Максимальное число участников"
+        v-model="maxUsersLimit"
+      />
+      <button type="submit" class="btn btn-primary">
+        Сохранить
+      </button>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -25,35 +48,7 @@ export default {
       maxUsersLimit: this.t.maxUsersLimit
     };
   },
-  methods: {
-    // Метод для корректного отображения даты последнего редактирования
-    stampToDate(stamp) {
-      let a = new Date(+stamp);
-      var months = [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec"
-      ];
-      var year = a.getFullYear();
-      var month = months[a.getMonth()];
-      var date = a.getDate();
-      var hour = a.getHours();
-      var min = a.getMinutes();
-      var sec = a.getSeconds();
-      var time =
-        date + " " + month + " " + year + " " + hour + ":" + min + ":" + sec;
-      return time;
-    }
-  }
+  methods: {}
 };
 </script>
 
