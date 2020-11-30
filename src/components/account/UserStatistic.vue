0ecf57ed16c0fd137552eaf29e34bf9e15fe138b
Elena Bychkova <lena.bychkova.200012@gmail.com>
2020-11-30 14:17:08 +0300

    DEV:The work with userStatistic was started

---
+++ src/components/account/UserStatistic.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h3>Личная статистика</h3>
+    <p>Заработанные баллы: {{ personalUserStatistics.pointQuantity }}</p>
+    <div
+      v-for="(statistic, index) in personalUserStatistics.pointsOperation"
+      :key="index"
+    >
+      <div>
+      <hr />
+      <p>Дата: {{$d(statistic.createdAt, "long")}}</p>
+      <p>Баллов: {{ personalUserStatistics.pointQuantity - statistic.delta }} за "{{ statistic.operationDescription }}"</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { PERSONAL_USER_STATISTIC_QUERY } from "@/graphql/queries";
+export default {
+  apollo: {
+    personalUserStatistics: {
+      query: PERSONAL_USER_STATISTIC_QUERY,
+      variables() {
+        return {
+          userId: 21 //this.$route.params.id
+        };
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
