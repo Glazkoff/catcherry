975eeb5e7ae364d303929c760cd93e02e50b7834
Nikita Glazkov <zitrnik@gmail.com>
2020-12-01 01:33:40 +0300

    DEV: change date format in i18n

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,45 +1,10 @@
 import Vue from "vue";
 import VueI18n from "vue-i18n";
 
-// import messages from "@/i18n/locales/ru.json";
 import getBrowserLocale from "@/i18n/get-browser-locale.js";
 import { supportedLocalesInclude } from "@/i18n/supported-locales.js";
 
 Vue.use(VueI18n);
-
-const dateTimeFormats = {
-  en: {
-    short: {
-      year: "numeric",
-      month: "short",
-      day: "numeric"
-    },
-    long: {
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-      weekday: "short",
-      hour: "numeric",
-      minute: "numeric"
-    }
-  },
-  ru: {
-    short: {
-      year: "numeric",
-      month: "short",
-      day: "numeric"
-    },
-    long: {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      // weekday: "short",
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric"
-    }
-  }
-};
 
 function loadLocaleMessages() {
   const locales = require.context(
@@ -56,6 +21,23 @@ function loadLocaleMessages() {
     }
   });
   return messages;
+}
+
+function loadLocaleDateTimeFormats() {
+  const locales = require.context(
+    "@/i18n/datetimeformats",
+    true,
+    /[A-Za-z0-9-_,\s]+\.json$/i
+  );
+  const dateTimeFormats = {};
+  locales.keys().forEach(key => {
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
+    if (matched && matched.length > 1) {
+      const locale = matched[1];
+      dateTimeFormats[locale] = locales(key);
+    }
+  });
+  return dateTimeFormats;
 }
 
 function getStartingLocale() {
@@ -77,7 +59,7 @@ function getStartingLocale() {
 export const i18n = new VueI18n({
   locale: getStartingLocale(),
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || "en",
-  dateTimeFormats,
+  dateTimeFormats: loadLocaleDateTimeFormats(),
   messages: loadLocaleMessages()
 });
 
