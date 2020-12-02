6e0727d486ce6ca89fa0114703f8346395c83972
Elena Bychkova <lena.bychkova.200012@gmail.com>
2020-12-02 14:53:34 +0300

    DEV: second screen in progress

--- src/components/account/Account.vue
+++ src/components/account/Account.vue
@@ -1,155 +1,10 @@
 <template>
-  <div class="main">
-    <popup v-if="isShowFullInformation">
-      <h3 slot="header" v-if="isShowModalEdit">
-        {{ $t("editUser") }}
-      </h3>
-      <div slot="body" v-if="isShowModalEdit">
-        <form @submit.prevent="saveUserOnPopup()">
-          <label for="surname">Фамилия</label>
-          <input
-            class="form-control form-text"
-            name="surname"
-            v-model.trim="$v.user.surname.$model"
-            @blur="$v.user.surname.$touch()"
-            placeholder="Фамилия"
-          />
-          <div v-if="$v.user.surname.$error" class="error">
-            <span v-if="!$v.user.surname.required"
-              >Данное поле обязательно</span
-            >
-            <span v-else-if="!$v.user.surname.alpha"
-              >Поле должно содержать только буквы</span
-            >
-          </div>
-          <label for="name">Имя</label>
-          <input
-            class="form-control form-text"
-            name="name"
-            v-model.trim="$v.user.name.$model"
-            placeholder="Имя"
-            required
-          />
-          <label for="patricity">Отчество (при наличии)</label>
-          <input
-            class="form-control form-text"
-            name="patricity"
-            v-model.trim="$v.user.patricity.$model"
-            placeholder="Отчество"
-            required
-          />
-          <label for="gender">Пол</label>
-          <select
-            class="form-control"
-            name="gender"
-            v-model.trim="$v.user.gender.$model"
-            required
-          >
-            <option>Мужской</option>
-            <option>Женский</option>
-          </select>
-          <label for="login">Логин</label>
-          <input
-            class="form-control form-text"
-            name="login"
-            v-model.trim="$v.user.login.$model"
-            placeholder="Логин"
-            required
-          />
-          <label for="password">Пароль</label>
-          <input
-            class="form-control form-text"
-            name="password"
-            v-model.trim="$v.user.password.$model"
-            placeholder="Пароль"
-            required
-          />
-        </form>
-      </div>
-      <div slot="footer" class="btn-group" v-if="isShowModalEdit">
-        <button
-          class="modal-default-button btn btn-primary"
-          @click="saveUserOnPopup()"
-        >
-          {{ $t("save") }}
-        </button>
-        <button class="btn btn-secondary" @click="cancelModal()">
-          {{ $t("cancel") }}
-        </button>
-      </div>
-
-      <h3 slot="header" v-if="isShowModalDelete">
-        {{ $t("deleteQuestion") }}
-        {{ fullName }}?
-      </h3>
-      <div slot="body" v-if="isShowModalDelete" class="btn-group">
-        <button
-          @click="deleteUser()"
-          slot="footer"
-          class="modal-default-button"
-        >
-          {{ $t("delete") }}
-        </button>
-        <button @click="cancelModal()">
-          {{ $t("cancel") }}
-        </button>
-      </div>
-
-      <h3
-        slot="header"
-        v-if="!isShowModalDelete && !isShowModalEdit && $apollo.loading"
-      >
-        Загрузка...
-      </h3>
-      <h3
-        slot="header"
-        v-if="!isShowModalDelete && !isShowModalEdit && !$apollo.loading"
-      >
-        Карточка пользователя
-      </h3>
-      <div
-        slot="body"
-        v-if="!isShowModalDelete && !isShowModalEdit && !$apollo.loading"
-      >
-        <p>Фамилия: {{ user.surname }}</p>
-        <p>Имя: {{ user.name }}</p>
-        <p>Отчетсво: {{ user.patricity }}</p>
-        <p>Пол: {{ user.gender }}</p>
-        <p>
-          Дата рождения:
-          {{
-            new Date(user.birthday).toLocaleString("ru", {
-              day: "numeric",
-              month: "long",
-              year: "numeric"
-            })
-          }}
-        </p>
-        <p>Логин: {{ user.login }}</p>
-        <p>Пароль: {{ user.password }}</p>
-      </div>
-      <div
-        slot="footer"
-        class="btn-group"
-        v-if="!isShowModalDelete && !isShowModalEdit && !$apollo.loading"
-      >
-        <button class="btn btn-primary" @click="showModalEdit()">
-          Редактировать
-        </button>
-        <button class="btn btn-link" @click="showModalDelete()">
-          Удалить
-        </button>
-        <button class="btn btn-secondary" @click="cancelFullInformation()">
-          {{ $t("close") }}
-        </button>
-      </div>
-    </popup>
-
-    <div class="double">
+  <div>
+    <div class="double" v-if="!isEdit">
       <div class="flexCont">
         <h2>{{ $t("profileUser") }}</h2>
         <h3 v-if="$apollo.loading">
-          Загрузка...
+          {{ $t("loading") }}
         </h3>
         <div v-if="!$apollo.loading">
           <h6 v-if="users.length == 0">
@@ -197,23 +52,116 @@
       </div>
       <div class="card">
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
-          quia natus. Dignissimos reprehenderit consectetur quo commodi
-          perspiciatis hic, aut nostrum,
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam
+          provident cupiditate, a culpa adipisci laudantium repudiandae
+          excepturi consequuntur architecto officiis ex, voluptatem est
+          obcaecati odit ipsum? Necessitatibus placeat animi pariatur.
         </p>
       </div>
     </div>
+
+    <!-- Редактирование пользователя -->
+    <div v-else class="double">
+      <div class="flexCont">
+        <h2>{{ $t("profileUser") }}</h2>
+        <img src="@/assets/avatar.jpg" alt="user" class="card_img" />
+        <p @click="deleteUser()" class="danger">
+          {{ $t("delete") }}
+        </p>
+      </div>
+      <div class="flexCont">
+        <h3>
+          {{ $t("editUser") }}
+        </h3>
+        <div>
+          <form @submit.prevent="saveUserOnPopup()">
+            <label for="surname">Фамилия</label>
+            <input
+              class="form-control form-text"
+              name="surname"
+              v-model.trim="$v.user.surname.$model"
+              @blur="$v.user.surname.$touch()"
+              placeholder="Фамилия"
+            />
+            <div v-if="$v.user.surname.$error" class="error">
+              <span v-if="!$v.user.surname.required"
+                >Данное поле обязательно</span
+              >
+              <span v-else-if="!$v.user.surname.alpha"
+                >Поле должно содержать только буквы</span
+              >
+            </div>
+            <label for="name">Имя</label>
+            <input
+              class="form-control form-text"
+              name="name"
+              v-model.trim="$v.user.name.$model"
+              placeholder="Имя"
+              required
+            />
+            <label for="patricity">Отчество (при наличии)</label>
+            <input
+              class="form-control form-text"
+              name="patricity"
+              v-model.trim="$v.user.patricity.$model"
+              placeholder="Отчество"
+              required
+            />
+            <label for="gender">Пол</label>
+            <select
+              class="form-control"
+              name="gender"
+              v-model.trim="$v.user.gender.$model"
+              required
+            >
+              <option>Мужской</option>
+              <option>Женский</option>
+            </select>
+            <label for="login">Логин</label>
+            <input
+              class="form-control form-text"
+              name="login"
+              v-model.trim="$v.user.login.$model"
+              placeholder="Логин"
+              required
+            />
+            <label for="password">Пароль</label>
+            <input
+              class="form-control form-text"
+              name="password"
+              v-model.trim="$v.user.password.$model"
+              placeholder="Пароль"
+              required
+            />
+          </form>
+        </div>
+        <div>
+          <button @click="saveUserOnPopup()">
+            {{ $t("save") }}
+          </button>
+        </div>
+      </div>
+
+      <h3 v-if="isShowModalDelete">
+        {{ $t("deleteQuestion") }}
+        {{ fullName }}?
+      </h3>
+      <div v-if="isShowModalDelete">
+        <button @click="cancelModal()">
+          {{ $t("cancel") }}
+        </button>
+      </div>
+    </div>
     <minialert v-if="isShowAlertEdit"
-      ><p slot="title">Вы успешно изменили пользователя</p></minialert
+      ><p slot="title">{{ $t("minialertEditUser") }}</p></minialert
     >
     <minialert v-if="isShowAlertDelete"
-      ><p slot="title">Вы успешно удалили пользователя</p></minialert
+      ><p slot="title">{{ $t("minialertDeleteUser") }}</p></minialert
     >
   </div>
 </template>
 
 <script>
-import popup from "@/components/Popup.vue";
 import minialert from "@/components/MiniAlert.vue";
 import Edit from "@/assets/account_edit.svg";
 import Points from "@/components/account/PointsUser.vue";
@@ -225,7 +173,7 @@ import {
   ONE_USER_QUERY
 } from "@/graphql/queries";
 export default {
-  components: { minialert, popup, Edit, Points },
+  components: { minialert, Edit, Points },
   apollo: {
     users: {
       query: USERS_QUERY
@@ -242,7 +190,7 @@ export default {
   data() {
     return {
       nameOfUser: "",
-      isShowFullInformation: false,
+      isEdit: false,
       index: 0,
       userId: -1,
       fullName: "",
@@ -282,7 +230,7 @@ export default {
   methods: {
     showFullInformation(id) {
       this.userId = id;
-      this.isShowFullInformation = true;
+      this.isEdit = true;
     },
     cancelFullInformation() {
       this.isShowFullInformation = false;
@@ -315,7 +263,7 @@ export default {
         .catch(error => {
           console.error(error);
         });
-      this.isShowFullInformation = false;
+      this.isEdit = false;
       this.isShowModalDelete = false;
       this.isShowAlertDelete = true;
       setTimeout(() => {
