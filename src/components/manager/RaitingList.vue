d3bd122da4f19542e707a60cacd5b619eff392aa
Nikita Dubinsky <nikitadubinsk@gmail.com>
2020-12-05 12:13:23 +0300

    DEV: add user s statistics

--- src/components/manager/RaitingList.vue
+++ src/components/manager/RaitingList.vue
@@ -21,102 +21,100 @@
         <div
           @click="showFullInformation(oneUser)"
           class="card_action"
-          v-if="!isShowFullInformation"
+          v-if="oneUser.user.id !== userId"
         >
           <ArrowRight></ArrowRight>
         </div>
         <div
           @click="closeFullInformation()"
           class="card_action"
-          v-if="isShowFullInformation"
+          v-if="isShowFullInformation && oneUser.user.id === userId"
         >
           <ArrowRight class="rotate"></ArrowRight>
         </div>
       </div>
       <div
         class="card_more"
-        v-if="isShowFullInformation && oneUser.user.id === userId"
+        v-if="
+          $apollo.loading && isShowFullInformation && oneUser.user.id === userId
+        "
       >
-        <h3>История</h3>
-        <p
-          v-for="pointsOperation in getOperationPointsUser"
-          :key="pointsOperation.id"
-        >
-          +{{ pointsOperation.delta }} балла(ов)
-          {{ pointsOperation.operationDescription }}
-        </p>
-        <h3>Статистика</h3>
-        <p>За текущую неделю:</p>
-        <p>{{ getPointsUser.pointQuantity }} баллов</p>
-        <p>На прошлой недле:</p>
-        <p>{{ pointsLastWeek }} баллов</p>
+        Загрузка...
       </div>
-    </div>
-    <!-- инпуты для смены периода отображения рейтинга  -->
-    В период с
-    <input type="date" v-model="weekAgo" class="form-control mini" /> по
-    <input type="date" v-model="today" class="form-control mini" /> в сравнении
-    с предыдущим
-    <select class="form-control mini" v-model="comparsionPeriod">
-      <option value="1">аналогичным</option>
-      <option value="2">недельным</option>
-      <option value="3">месячным</option>
-    </select>
-    периодом
-    <hr />
-    <a @click="sortList()" class="btn btn-link">По общему количеству</a>
-    <a @click="weeklyList()" class="btn btn-link">По результату периода</a>
-    <div v-for="raiting in raitingInTeams" :key="raiting.id" class="oneUser">
-      <img src="@/assets/avatar.jpg" alt="photo" class="mediumAvatar" />
-      <p>
-        <b>{{ raiting.user.name }}</b>
-      </p>
-      <p>Заработанные баллы: {{ raiting.user.userPoints.pointQuantity }}</p>
-      <div>
-        За период заработано:
-        <!-- функция отображения баллов пользователя -->
-        <div
-          v-if="
-            +summOperations(raiting.user.userPoints.pointsOperation).summ > 0
-          "
-          class="up"
-        >
-          ↑ +{{ summOperations(raiting.user.userPoints.pointsOperation).summ }}
-        </div>
-        <div
-          v-else-if="
-            +summOperations(raiting.user.userPoints.pointsOperation).summ < 0
-          "
-          class="down"
-        >
-          ↓ {{ summOperations(raiting.user.userPoints.pointsOperation).summ }}
-        </div>
-        <div v-else>0</div>
+      <div
+        class="card_more"
+        v-if="
+          isShowFullInformation &&
+            oneUser.user.id === userId &&
+            !$apollo.loading
+        "
+      >
         <div>
-          Процентный показатель:
-          {{ summOperations(raiting.user.userPoints.pointsOperation).percent }}%
+          <h3>История</h3>
+          <p
+            v-for="pointsOperation in getOperationPointsUser"
+            :key="pointsOperation.id"
+            class="history"
+          >
+            <small v-if="pointsOperation.delta > 0">+</small
+            >{{ pointsOperation.delta }} балла(ов)
+            {{ pointsOperation.operationDescription }}
+          </p>
+        </div>
+        <div>
+          <h3>Статистика</h3>
+          <div class="statistics">
+            <div>
+              <p>За текущую неделю:</p>
+              <p
+                class="points_now"
+                :class="{
+                  more: pointsLastWeek[0] > pointsLastWeek[1],
+                  less: pointsLastWeek[0] < pointsLastWeek[1]
+                }"
+              >
+                <userStatisticsDown
+                  v-show="pointsLastWeek[0] < pointsLastWeek[1]"
+                ></userStatisticsDown>
+                <userStatisticsUp
+                  v-show="pointsLastWeek[0] > pointsLastWeek[1]"
+                ></userStatisticsUp>
+                {{ pointsLastWeek[0] }} баллов
+              </p>
+              <small v-if="pointsLastWeek[0] > pointsLastWeek[1]">
+                на {{ pointsLastWeek[0] - pointsLastWeek[1] }} баллов больше,
+                чем за предыдущую неделю
+              </small>
+              <small v-if="pointsLastWeek[0] < pointsLastWeek[1]">
+                на {{ pointsLastWeek[1] - pointsLastWeek[0] }} баллов меньше,
+                чем за предыдущую неделю
+              </small>
+            </div>
+            <div>
+              <p>На прошлой неделе:</p>
+              <p v-if="pointsLastWeek !== null" class="points_now">
+                {{ pointsLastWeek[1] }} баллов
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
-
 <script>
 import ArrowRight from "@/assets/svg/admin/arrow_right.svg?inline";
+import userStatisticsDown from "@/assets/svg/manager/userStatisticsDown.svg?inline";
+import userStatisticsUp from "@/assets/svg/manager/userStatisticsUp.svg?inline";
 import {
-  RAITING_IN_TEAMS_QUERY,
   USERS_IN_TEAMS_QUERY,
   GET_POINTS_OPERATION_QUERY,
-  GET_POINTS_QUERY,
   GET_POINTS_LAST_WEEK_QUERY
 } from "@/graphql/queries";
 export default {
-  components: { ArrowRight },
+  components: { ArrowRight, userStatisticsDown, userStatisticsUp },
   data() {
     return {
-      today: this.stampToDate(new Date()),
-      weekAgo: this.stampToDate(new Date().setDate(new Date().getDate() - 7)),
-      comparsionPeriod: 1,
       isShowFullInformation: false,
       userId: 0
     };
@@ -145,23 +143,6 @@ export default {
           pointAccountId: this.userId
         };
       }
-    },
-    getPointsUser: {
-      query: GET_POINTS_QUERY,
-      variables() {
-        return {
-          userId: this.userId
-        };
-      }
-    },
-    // массив пользователей команды с рейтиногм по баллам
-    raitingInTeams: {
-      query: RAITING_IN_TEAMS_QUERY,
-      variables() {
-        return {
-          teamId: this.$route.params.id
-        };
-      }
     }
   },
   methods: {
@@ -170,163 +151,8 @@ export default {
       this.isShowFullInformation = true;
     },
     closeFullInformation() {
-      this.isShowFullInformation = true;
+      this.isShowFullInformation = false;
       this.userId = 0;
-    },
-    // сортировка по общему количеству баллов
-    sortList() {
-      this.raitingInTeams.sort(
-        (a, b) =>
-          parseFloat(b.user.userPoints.pointQuantity) -
-          parseFloat(a.user.userPoints.pointQuantity)
-      );
-    },
-    // сортировка по недельному количеству заработанных баллов
-    weeklyList() {
-      this.raitingInTeams.sort(
-        (a, b) =>
-          parseFloat(this.summOperations(b.user.userPoints.pointsOperation)) -
-          parseFloat(this.summOperations(a.user.userPoints.pointsOperation))
-      );
-    },
-    // сумма баллов по заданному временному промежутку
-    summOperations(pointsOperation) {
-      let summ = 0;
-      let summCurrent = 0;
-      let summComparsion = 0;
-      for (let i = 0; i < pointsOperation.length; i++) {
-        if (
-          this.stampToDate(+pointsOperation[i].createdAt) <
-            this.stampToDate(this.today) &&
-          this.stampToDate(+pointsOperation[i].createdAt) >=
-            this.stampToDate(this.weekAgo)
-        ) {
-          //сумма баллов в установленном периоде
-          summCurrent += pointsOperation[i].delta;
-        }
-      }
-
-      if (this.comparsionPeriod == 1) {
-        //аналогичным периодом
-        console.log(this.stampToDate(this.weekAgo));
-        console.log(
-          this.stampToDate(
-            new Date().setDate(
-              new Date(this.weekAgo).getDate() -
-                (new Date(this.today).getDate() -
-                  new Date(this.weekAgo).getDate())
-            )
-          )
-        );
-
-        for (let i = 0; i < pointsOperation.length; i++) {
-          if (
-            this.stampToDate(+pointsOperation[i].createdAt) <
-              this.stampToDate(this.weekAgo) &&
-            this.stampToDate(+pointsOperation[i].createdAt) >=
-              this.stampToDate(
-                new Date().setDate(
-                  new Date(this.weekAgo).getDate() -
-                    (new Date(this.today).getDate() -
-                      new Date(this.weekAgo).getDate())
-                )
-              )
-          ) {
-            //если дата операции меньше даты начала установленного периода И
-            //если дата операции больше даты начала предыдущего установленному периоду
-            summComparsion += pointsOperation[i].delta;
-          }
-        }
-      }
-
-      if (this.comparsionPeriod == 2) {
-        //недельным периодом
-        console.log(
-          this.stampToDate(
-            new Date(this.today).setDate(new Date(this.today).getDate() - 7)
-          )
-        );
-        console.log(
-          this.stampToDate(
-            new Date(this.weekAgo).setDate(new Date(this.weekAgo).getDate() - 7)
-          )
-        );
-
-        for (let i = 0; i < pointsOperation.length; i++) {
-          if (
-            this.stampToDate(+pointsOperation[i].createdAt) <
-              this.stampToDate(
-                new Date(this.today).setDate(new Date(this.today).getDate() - 7)
-              ) &&
-            this.stampToDate(+pointsOperation[i].createdAt) >=
-              this.stampToDate(
-                new Date(this.weekAgo).setDate(
-                  new Date(this.weekAgo).getDate() - 7
-                )
-              )
-          ) {
-            //если дата операции меньше даты конца установленного периода недельной давности И
-            //если дата операции больше начала значения установленного периода недельной давности
-            summComparsion += pointsOperation[i].delta;
-          }
-        }
-      }
-
-      if (this.comparsionPeriod == 3) {
-        //недельным периодом
-        console.log(
-          this.stampToDate(
-            new Date(this.today).setMonth(new Date(this.today).getMonth() - 1)
-          )
-        );
-        console.log(
-          this.stampToDate(
-            new Date(this.weekAgo).setMonth(
-              new Date(this.weekAgo).getMonth() - 1
-            )
-          )
-        );
-
-        for (let i = 0; i < pointsOperation.length; i++) {
-          if (
-            this.stampToDate(+pointsOperation[i].createdAt) <
-              this.stampToDate(
-                new Date(this.today).setMonth(
-                  new Date(this.today).getMonth() - 1
-                )
-              ) &&
-            this.stampToDate(+pointsOperation[i].createdAt) >=
-              this.stampToDate(
-                new Date(this.weekAgo).setMonth(
-                  new Date(this.weekAgo).getMonth() - 1
-                )
-              )
-          ) {
-            //если дата операции меньше даты конца установленного периода месячной давности И
-            //если дата операции больше начала значения установленного периода месячной давности
-            summComparsion += pointsOperation[i].delta;
-          }
-        }
-      }
-      summ = summCurrent - summComparsion; //текущая сумма минус предыдущая сумма
-      let percent;
-      if (summComparsion != 0) {
-        percent = ((summCurrent * 100) / summComparsion - 100).toFixed(2); //текущая сумма *100 поделена на предыдущую сумму с вычетом 100
-      } else {
-        percent = 0;
-      }
-      return { summ, percent };
-    },
-    // отображение тайм-штампа в фотмате для input date
-    stampToDate(stamp) {
-      let a = new Date(stamp);
-      let year = a.getFullYear();
-      let month = a.getMonth() + 1;
-      if (month < 10) month = "0" + month;
-      let date = a.getDate();
-      if (date < 10) date = "0" + date;
-      let time = year + "-" + month + "-" + date;
-      return time;
     }
   }
 };
@@ -372,5 +198,32 @@ export default {
 .rotate {
   transform: rotate(0.25turn);
   transition: 0.25s;
+}
+
+.card_more small {
+  color: $gray_3;
+  padding: 0;
+  margin: 0;
+}
+
+.points_now {
+  font-size: 28px;
+  font-weight: 500;
+  padding: 0;
+  margin: 0 0 5px 0;
+}
+
+.more {
+  color: $blue;
+}
+
+.less {
+  color: $red;
+}
+
+.statistics {
+  display: grid;
+  grid-template-columns: 47% 47%;
+  grid-column-gap: 6%;
 }
 </style>
