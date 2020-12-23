efe7deb80ba8dcfb8e9d2f7c24d134910b3d0b9d
nvkolezneva <nvkolezneva@gmail.com>
2020-12-23 12:06:23 +0300

    FIX:pluralization for points and points operation

--- src/i18n/pluralizationrules/ru.js
+++ src/i18n/pluralizationrules/ru.js
@@ -1,21 +1,22 @@
 export default function(choice, choicesLength) {
-  if (choice === 0) {
+  if (Math.abs(choice) === 0) {
     return 0;
   }
 
   const teen =
-    (choice > 10 && choice < 20) || (choice > 10 && choice % 100 < 20);
-  const endsWithOne = choice % 10 === 1;
+    (Math.abs(choice) > 10 && Math.abs(choice) < 20) ||
+    (Math.abs(choice) % 100 > 10 && Math.abs(choice) % 100 < 20);
+  const endsWithOne = Math.abs(choice) % 10 === 1;
 
-  if (choicesLength < 4) {
+  if (Math.abs(choicesLength) < 4) {
     return !teen && endsWithOne ? 1 : 2;
   }
   if (!teen && endsWithOne) {
     return 1;
   }
-  if (!teen && choice % 10 >= 2 && choice % 10 <= 4) {
+  if (!teen && Math.abs(choice) % 10 >= 2 && Math.abs(choice) % 10 <= 4) {
     return 2;
   }
 
-  return choicesLength < 4 ? 2 : 3;
+  return Math.abs(choicesLength) < 4 ? 2 : 3;
 }
